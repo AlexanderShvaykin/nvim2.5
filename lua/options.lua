19a50408267846a9bbd68b512ce84c46f5db250c
Alexander Shvaykin <skiline.alex@gmail.com>
2025-03-06 18:50:50 +0300

    add rails find api

--- lua/options.lua
+++ lua/options.lua
@@ -5,3 +5,14 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.cmd("syntax off")
+vim.cmd([[let g:rails_projections = {
+\ "app/api/*.rb": {
+\   "command": "api",
+\   "test": [
+\     "spec/requests/{}_spec.rb"
+\   ],
+\ },
+\ "packages/*/lib/engine.rb": {
+\   "command": "package",
+\ }}
+]])
