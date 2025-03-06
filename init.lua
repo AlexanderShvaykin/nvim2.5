b0909bd41bebfe54e4fb55af5ff8486d31491d14
Alexander Shvaykin <skiline.alex@gmail.com>
2025-03-06 18:51:21 +0300

    setup makefile syntaz

--- init.lua
+++ init.lua
@@ -32,6 +32,26 @@ dofile(vim.g.base46_cache .. "statusline")
 require "options"
 require "nvchad.autocmds"
 
+-- Set up indentation for Makefiles
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    -- Set tab width to 4 spaces
+    vim.opt_local.tabstop = 4
+    -- Set shiftwidth to 4 spaces
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+-- Create a BufLeave autocmd to reset settings when leaving Makefile buffers
+vim.api.nvim_create_autocmd("BufLeave", {
+  pattern = "*/Makefile",
+  callback = function()
+    vim.opt_local.tabstop = 2       -- Your default tab size
+    vim.opt_local.shiftwidth = 2    -- Your default shift width
+  end,
+})
+
 vim.schedule(function()
   require "mappings"
 end)
